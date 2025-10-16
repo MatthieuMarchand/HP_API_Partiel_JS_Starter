13820e2c57f9a31be64f332e24b76e0ec4bc341a
Matthieu <matthieu.marchand@mmibordeaux.com>
2025-10-16 11:32:33 +0200

    [Partie 2.2] En cliquant sur une maison, ça doit trier parmi les personnages présents et par ordre alphabétique

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,62 @@
 const CHARACTER_LIMIT = 12;
-const API_HOGWARTS_STUDENTS =
-  "https://hp-api.onrender.com/api/characters/students";
+const API_ALL_CHARACTERS = "https://hp-api.onrender.com/api/characters";
 
 const CharactersHTML = document.getElementById("characters");
 
-async function getHogwartsStudents() {
+async function getCharacters() {
   try {
-    const response = await fetch(API_HOGWARTS_STUDENTS);
-    const students = await response.json();
-    return students.slice(0, CHARACTER_LIMIT);
+    const response = await fetch(API_ALL_CHARACTERS);
+    const characters = await response.json();
+    return characters
+      .slice(0, CHARACTER_LIMIT)
+      .sort((a, b) => a.name.localeCompare(b.name));
   } catch (error) {
     console.error(error.message);
   }
 }
 
-async function displayStudents() {
-  const students = await getHogwartsStudents();
+function displayCharacters(characters) {
+  CharactersHTML.innerHTML = "";
+  characters.forEach((character) => {
+    if (!character.image) return;
 
-  students.forEach((student) => {
-    const studentDiv = document.createElement("div");
-    if (student.house) {
-      studentDiv.classList.add(student.house.toLowerCase());
+    const characterDiv = document.createElement("div");
+    if (character.house) {
+      characterDiv.classList.add(character.house.toLowerCase());
     }
 
-    studentDiv.innerHTML = `
-        <img src="${student.image}" alt="Portrait of ${student.name}" />
-        <p>${student.name}</p>
+    characterDiv.innerHTML = `
+        <img src="${character.image}" alt="Portrait of ${character.name}" />
+        <p>${character.name}</p>
     `;
 
-    CharactersHTML.appendChild(studentDiv);
+    CharactersHTML.appendChild(characterDiv);
   });
 }
 
-displayStudents();
+function filterByHouse(house, characters) {
+  if (house === "all") {
+    return characters;
+  }
+  return characters.filter(
+    (character) =>
+      character.house && character.house.toLowerCase() === house.toLowerCase()
+  );
+}
+
+async function init() {
+  const characters = await getCharacters();
+
+  const houseButtons = document.querySelectorAll(".house-blason");
+  houseButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const house = btn.dataset.house;
+      const filteredCharacters = filterByHouse(house, characters);
+      displayCharacters(filteredCharacters);
+    });
+  });
+
+  displayCharacters(characters);
+}
+
+init();
